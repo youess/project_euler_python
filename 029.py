977109a0c23154a53e81d157c1d5dc3f43a3182b
youess <denis_lton@hotmail.com>
2015-08-27 12:45:00 +0800

    smart soluton without big integer

--- 029.py
+++ 029.py
@@ -32,6 +32,40 @@ def get_distinct_power_num(a, b):
     return len(s)
 
 
+def get_tuple(i, j):
+
+    '''
+    >>> get_tuple(8, 2)
+    (2, 6)
+    >>> get_tuple(49, 2)
+    (7, 4)
+    '''
+    base = [2, 3, 5, 6, 7, 10]
+    while base:
+        b = base.pop()
+        index = 0
+        tmp = i
+        while tmp / b:
+            index += 1
+            tmp /= b
+        if b**index == i:
+            j *= index
+            i = b
+            break
+    return (i, j)
+
+
+def get_distinct_power_num2(a, b):
+    '''
+    >>> get_distinct_power_num2(100, 100)
+    9183
+    '''
+    s = set()
+    for i in xrange(2, a+1):
+        for j in xrange(2, b+1):
+            s.add(get_tuple(i, j))
+    return len(s)
+
 
 if __name__ == "__main__":
     doctest.testmod()
